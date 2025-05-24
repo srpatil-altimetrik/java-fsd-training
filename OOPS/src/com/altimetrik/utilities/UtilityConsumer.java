bc43a4f2c9919fe0716abf8ca721f79a6f87a3f2
srpatil <srpatil@AIPL6457LTIN603.altimetrik.com>
2025-05-24 23:54:59 +0530

    Some daily utility classes

---
+++ OOPS/src/com/altimetrik/utilities/UtilityConsumer.java
@@ -0,0 +1,30 @@
+package com.altimetrik.utilities;
+
+import java.util.ArrayList;
+import java.util.function.Consumer;
+
+
+class MyConsumer<T> implements Consumer<T> {
+	public void accept(T t) {
+		System.out.println("Consumed: " + t);
+		System.out.println("Result after adding 10 : "+ ((int)t+10));
+	}
+}
+
+
+public class UtilityConsumer {
+	public static void main(String[] args) {
+		ArrayList list = new ArrayList<>(5);
+		
+		Consumer myConsumer = new MyConsumer();
+		
+		list.add(100);
+		list.add(300);
+		list.add(500);
+		list.add(700);
+		list.add(900);
+		System.out.println("ArrayList elements: " + list);
+		
+		list.forEach(myConsumer);
+	}
+}
---
+++ OOPS/src/com/altimetrik/utilities/UtilityStringTokenizer.java
@@ -0,0 +1,33 @@
+package com.altimetrik.utilities;
+
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class UtilityStringTokenizer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		StringTokenizer st = new StringTokenizer(str, " "); // Create a StringTokenizer to split the string by spaces
+		System.out.println("StringTokenizer output:"+ st.countTokens()); // Print the number of tokens in the string
+		int totalCount = 0;
+		while (st.hasMoreTokens()) { // Check if there are more tokens
+			String word = st.nextToken(); // Get the next token
+			System.out.println("Word: " + word);
+			int vowelCount = 0;
+			
+			for(char ch : word.toCharArray()) { // Iterate through each character in the word
+				ch = Character.toLowerCase(ch); // Convert character to lowercase for case-insensitive comparison
+				if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+					vowelCount++; // Increment the vowel count if the character is a vowel
+					totalCount++; // Increment the total vowel count
+				}
+				
+			}
+			System.out.println("Number of vowels in '" + word + "': " + vowelCount);
+		}
+		System.out.println("Total number of vowels in the string: " + totalCount);
+		sc.close();
+	}
+}

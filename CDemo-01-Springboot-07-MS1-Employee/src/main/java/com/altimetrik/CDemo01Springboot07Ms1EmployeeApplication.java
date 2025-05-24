f54eed67efbad43a9276dcab1aa8e0f8d646efb3
srpatil <srpatil@AIPL6457LTIN603.altimetrik.com>
2025-05-24 20:59:41 +0530

    Commit 15

---
+++ CDemo-01-Springboot-07-MS1-Employee/src/main/java/com/altimetrik/CDemo01Springboot07Ms1EmployeeApplication.java
@@ -0,0 +1,13 @@
+package com.altimetrik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CDemo01Springboot07Ms1EmployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CDemo01Springboot07Ms1EmployeeApplication.class, args);
+	}
+
+}

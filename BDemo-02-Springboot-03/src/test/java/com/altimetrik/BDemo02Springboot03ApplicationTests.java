5789e130f9beb14ca7ea346cd37d55882c00e2bb
srpatil <srpatil@AIPL6457LTIN603.altimetrik.com>
2025-05-24 20:28:57 +0530

    Commit 11

---
+++ BDemo-02-Springboot-03/src/test/java/com/altimetrik/BDemo02Springboot03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.altimetrik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BDemo02Springboot03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

110d381c0c6d226a9bd7aae55f4da7651cdf7e13
srpatil <srpatil@AIPL6457LTIN603.altimetrik.com>
2025-05-24 20:30:28 +0530

    Commit 14

---
+++ BDemo-03-Springboot-06/src/main/java/com/altimetrik/BDemo03Springboot06Application.java
@@ -0,0 +1,13 @@
+package com.altimetrik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BDemo03Springboot06Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BDemo03Springboot06Application.class, args);
+	}
+
+}
---
+++ BDemo-03-Springboot-06/src/main/java/com/altimetrik/controller/MyController.java
@@ -0,0 +1,23 @@
+package com.altimetrik.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+	
+	@GetMapping("/home")
+	public String home() {
+		return "<h1>Welcome to Spring Boot Application</h1>";
+	}
+	
+	@GetMapping("/login")
+	public String login() {
+		return "<h1>Login Page</h1>";
+	}
+	
+	@GetMapping("/sign-up")
+	public String signUp() {
+		return "<h1>Sign Up Page</h1>";
+	}
+}

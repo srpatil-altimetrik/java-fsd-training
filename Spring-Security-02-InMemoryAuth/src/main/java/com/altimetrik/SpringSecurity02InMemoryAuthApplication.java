056256190daf4ad0bca7bf1fd19494bf320430f0
srpatil <srpatil@AIPL6457LTIN603.altimetrik.com>
2025-06-05 10:33:20 +0530

    In Memory Auth Impl with password encoding concept

---
+++ Spring-Security-02-InMemoryAuth/src/main/java/com/altimetrik/SpringSecurity02InMemoryAuthApplication.java
@@ -0,0 +1,13 @@
+package com.altimetrik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurity02InMemoryAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurity02InMemoryAuthApplication.class, args);
+	}
+
+}

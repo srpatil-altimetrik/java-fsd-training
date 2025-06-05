fe6e0495241acc505f6a0f9d386d47ba4c150955
srpatil <srpatil@AIPL6457LTIN603.altimetrik.com>
2025-06-05 14:32:44 +0530

    Use of Database to store the credentials.

---
+++ Spring-Security-04-H2Database/src/main/java/com/altimetrik/SecurityConfig.java
@@ -0,0 +1,51 @@
+package com.altimetrik;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+	@Autowired
+	DataSource dataSource; // To get the database access and used those beans for this application
+
+	@Bean
+	SecurityFilterChain defailtSecurityFilterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests((requests) -> requests.requestMatchers("/h2-console/**").permitAll() //It permits the all request coming towards 																							// h2-console
+				.anyRequest().authenticated());
+		http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+//		http.formLogin(withDefaults());
+		http.httpBasic(withDefaults());
+		http.headers(headers -> headers.frameOptions(frameOptions -> frameOptions.sameOrigin()));
+		http.csrf(csrf -> csrf.disable());
+		return http.build();
+	}
+
+	@Bean
+	public UserDetailsService userDetailsService() {
+		UserDetails user = User.withUsername("user").password("{noop}Pass@1234").roles("USER").build();
+		UserDetails admin = User.withUsername("admin").password("{noop}Pass@1234").roles("ADMIN").build();
+
+		JdbcUserDetailsManager userDetailsManager = new JdbcUserDetailsManager(dataSource);
+		userDetailsManager.createUser(user);
+		userDetailsManager.createUser(admin);
+		return userDetailsManager;
+	}
+}
---
+++ Spring-Security-04-H2Database/src/main/java/com/altimetrik/SpringSecurity04InMeroryH2DatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.altimetrik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurity04InMeroryH2DatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurity04InMeroryH2DatabaseApplication.class, args);
+	}
+
+}

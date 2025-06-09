fe77cb6366bb88696e3cd67939bc3fabf5172270
srpatil <srpatil@AIPL6457LTIN603.altimetrik.com>
2025-06-09 14:59:30 +0530

    JWT in Spring

---
+++ Spring-Security-05-JWT/src/main/java/com/altimetrik/SpringSecurity05JwtApplication.java
@@ -0,0 +1,13 @@
+package com.altimetrik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurity05JwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurity05JwtApplication.class, args);
+	}
+
+}
